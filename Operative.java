c5ee50f2682a31fe33dd136d3374b345bbb0fb14
Kastus-Kochubey <71447227+Kastus-Kochubey@users.noreply.github.com>
2020-10-15 00:26:51 +0500

    Add files via upload

---
+++ Operative.java
@@ -0,0 +1,37 @@
+package Module1;
+
+import java.util.Scanner;
+
+public class Operative {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int a = in.nextInt();
+    }
+}
+
+class Figure {
+    void getArea(){
+
+    }
+}
+
+class Circle extends Figure {
+    @Override
+    Integer getArea(int posx, int posy, int radius){
+        return
+    }
+}
+
+class Triangle extends Figure {
+    @Override
+    Integer getArea(int posx, int posy){
+
+    }
+}
+
+class Square extends Figure {
+    @Override
+    Integer getArea(int posx, int posy){
+
+    }
+}
