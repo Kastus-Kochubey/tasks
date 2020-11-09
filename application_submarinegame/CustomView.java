482e02216c02476cd7b42242021f4c34775be289
Kastus-Kochubey <71447227+Kastus-Kochubey@users.noreply.github.com>
2020-11-09 20:02:44 +0500

    Add files via upload

--- application_submarinegame/CustomView.java
+++ application_submarinegame/CustomView.java
@@ -3,19 +3,27 @@ package com.example.application_submarinegame;
 import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Paint;
+import android.util.Log;
+import android.view.MotionEvent;
 import android.view.View;
 
+import static android.content.ContentValues.TAG;
+
 public class CustomView extends View {
-    int numberHorCells = getWidth() / 30;
-    int numberVertCells = getHeight() / 30;
+    int cellsSize = 40;
+    int numberHorCells = getWidth() / cellsSize;
+    int numberVertCells = getHeight() / cellsSize;
+    float density;
+    float vertCellSize;
+    float horCellSize;
 
     public CustomView(Context context) {
         super(context);
     }
 
-    public CustomView(Context context) {
+    public CustomView(Context context, float density) {
         super(context);
-        this.density =
+        this.density = density;
     }
 
 //    public CustomView(int numberHorCells, int numberVertCells) {
@@ -28,18 +36,28 @@ public class CustomView extends View {
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
-        numberHorCells = (getWidth() % 30 == 0) ? getWidth() / 30 - 1 : getWidth() / 30;
-        numberVertCells = (getHeight() % 30 == 0) ? getHeight() / 30 - 1 : getHeight() / 30;
+        numberHorCells = (getWidth() % cellsSize == 0) ? getWidth() / cellsSize - 1 : getWidth() / cellsSize;
+        numberVertCells = (getHeight() % cellsSize == 0) ? getHeight() / cellsSize - 1 : getHeight() / cellsSize;
+        vertCellSize = (float) getHeight() / numberVertCells;
+        horCellSize = (float) getWidth() / numberHorCells;
         Paint paint = new Paint();
         paint.setStrokeWidth(density);
 
         for (int x = 0; x < numberVertCells; x++) {
-            canvas.drawLine(x * 30, 0, x * 30, getWidth(), paint);
+            canvas.drawLine(x * horCellSize, 0, x * horCellSize, getHeight(), paint);
 
         }
-        for (int y = 0; y < numberVertCells - 1; y++) {
-            canvas.drawLine(0, y * 30, 0, getHeight(), paint);
-
+        for (int y = 0; y < numberVertCells + 1; y++) {
+            canvas.drawLine(0, y * vertCellSize, getWidth(), y * vertCellSize, paint);
         }
+    }
+
+
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        Log.d(TAG, String.format("onTouchEvent: X: %f Y:%f", event.getX(), event.getY()));
+
+        return super.onTouchEvent(event);
     }
 }
--- application_submarinegame/MainActivity.java
+++ application_submarinegame/MainActivity.java
@@ -2,10 +2,12 @@ package com.example.application_submarinegame;
 
 import android.content.pm.ActivityInfo;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
 import android.view.Display;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.util.Arrays;
 import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
@@ -13,15 +15,19 @@ public class MainActivity extends AppCompatActivity {
     int displayHeight;
     //    int cellWidth;
 //    int cellHeight;
-    int cellsVertNumber;
-    int cellsHorNumber;
+    int numberVertCells;
+    int numberHorCells;
+    float density;
+    int cellSize = 40;
+    float vertCellSize;
+    float horCellSize;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 //        setContentView(R.layout.activity_main);
-        CustomView view = new CustomView(getApplicationContext());
+        CustomView view = new CustomView(getApplicationContext(), density);
         setContentView(view);
 //        setContentView(CustomView);
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
@@ -30,33 +36,36 @@ public class MainActivity extends AppCompatActivity {
         displayWidth = display.getWidth();
         displayHeight = display.getHeight();
 
-        cellsHorNumber = (displayWidth % 30 == 0) ? displayWidth / 30 - 1 : displayWidth / 30;
-        cellsVertNumber = (displayHeight % 30 == 0) ? displayHeight / 30 - 1 : displayHeight / 30;
+        DisplayMetrics metrics = new DisplayMetrics();
+        density = metrics.density;
+        numberHorCells = (displayWidth % cellSize == 0) ? displayWidth / cellSize - 1 : displayWidth / cellSize;
+        numberVertCells = (displayHeight % cellSize == 0) ? displayHeight / cellSize - 1 : displayHeight / cellSize;
+        vertCellSize = (float) displayWidth / numberVertCells;
+        horCellSize = (float) displayHeight / numberHorCells;
+        System.out.println(numberHorCells);
+        System.out.println(numberVertCells);
        // float density = getResources().getDisplayMetrics().density плотность пикселей - исп для размера клеток и ширины линий
 
 //        cellWidth = displayWidth;
 //        cellHeight = displayHeight;
 
 
+//        drawGrid();
     }
 
-    protected void drawGrid() {
-        // draw grid
+
+
+    protected void makeSubmarineCoords() {
         Random r = new Random();
 //        r.nextInt((max - min) + 1) + min;
-        int[][] fieldOfCells = new int[cellsVertNumber][cellsHorNumber];
-        fieldOfCells[r.nextInt(cellsVertNumber)][r.nextInt(cellsHorNumber)] = 1;
+        int[][] fieldOfCells = new int[numberVertCells][numberHorCells];
+        fieldOfCells[r.nextInt(numberVertCells)][r.nextInt(numberHorCells)] = 1;
+        for (int i = 0; i < numberVertCells; i++) {
+                System.out.println(Arrays.toString(fieldOfCells[i]));
 
+        }
+        }
 
-//        setContentView(CustomView);
 
     }
 
-
-//    public void click(View v) {
-//        TextView text = findViewById(R.id.textView);
-//
-//
-//    }
-
-}
